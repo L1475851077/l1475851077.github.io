06e7ff7fe870732c6d4486e73fe176532e0b3201
LSZAN <1475851077@qq.com>
2026-03-10 15:41:03 +0800

    Update list item fridge-b2000 via CMS

--- products/data.js
+++ products/data.js
@@ -13,7 +13,7 @@ const CATEGORIES = [
 PRODUCTS = [
     {
         'id': 'fridge-b2000',
-        'name': 'testCommercial Double Door Fridge B-2000',
+        'name': '冰箱Commercial Double Door Fridge B-2000',
         'description': '304-5 Stainless Steel · Energy Saving · 3-Year Warranty',
         'category': 'fridge',
         'image': './product_images/images/img_1773122287519.jpg'
